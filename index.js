ce080673a5d7f239353e32751eee78701916bc46
Maggie-karuri <karurimargaret111@gmail.com>
2024-03-20 22:39:29 +0300

    function specification

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// code your solution here
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`;
+    }
+    console.log(saturdayFun());
+    saturdayFun('bathe my dog');
+function mondayWork(activity = "go to the office") {
+    return `This Monday, I will ${activity}.`;
+    }
+    console.log(mondayWork());
+function wrapAdjective(symbol) {
+    return function(adjective) {
+        return `You are ${symbol}${adjective}${symbol}!`;
+    }
+}
+let wrapStar = wrapAdjective('*');
+let emphaticStar = wrapStar("a hard worker");
+console.log(emphaticStar);
+let wrapPipe = wrapAdjective("||");
+let emphaticPipe = wrapPipe("a dedicated programmer");
+console.log(emphaticPipe); 
